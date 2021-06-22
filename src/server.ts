00e8653b7ce6c6fb2ca7be28a2de0a5e988a7eb4
Luis Bruno <l.brunofidelis@gmail.com>
2021-06-22 07:53:00 -0300

    Adding database configs

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
+import "reflect-metadata";
 import express from "express";
+
+import "./database";
 
 // @types/express
 const app = express();
 
-app.get("/test", (request, response) => {
-    return response.send("Okayz");
-});
+
 
 app.listen(3001, () => { console.log('Server is running on port 3001') } );
